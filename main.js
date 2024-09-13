2549d4f7d068328c1159f6bfef12364ab7295664
Sharique Akram <sharique.akram@jellyfish.co>
2024-09-13 18:08:39 -0400

    Updated css and updated game name

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
 import './style.css'
+import Phaser from 'phaser'
 
+const sizes ={
+  width: 500,
+  height: 500
+}
+
+const config = {
+  type: Phaser.WEBGL,
+  width: 500,
+  height: 500,
+  canvas:gameCanvas
+}
+
+const game = new Phaser.Game(config);
